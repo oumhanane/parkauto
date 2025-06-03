c203ebab911199b57c9fbcd928368fd131af9ff5
Saliha Hammad Ichou <oum.hanane@gmail.com>
2025-06-03 22:04:13 +0200

    ajout du déploiement de l'app avec Terraform et des manifests k8s

---
+++ app-deploy.tf
@@ -0,0 +1,9 @@
+# provider "kubernetes" {
+#  config_path = "~/.kube/config"
+# }
+
+resource "kubernetes_manifest" "app_manifests" {
+  for_each = fileset("${path.module}/k8s/terraform", "*.yaml")
+
+  manifest = yamldecode(file("${path.module}/k8s/terraform/${each.key}"))
+}
