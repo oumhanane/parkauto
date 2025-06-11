053ada01f6dc374dfe3d472559db48a2835226b4
Saliha Hammad Ichou <oum.hanane@gmail.com>
2025-06-11 13:52:01 +0200

    modif pipeline

--- microauto/client/src/main/java/com/parkauto/client/controller/ClientController.java
+++ microauto/client/src/main/java/com/parkauto/client/controller/ClientController.java
@@ -4,7 +4,7 @@ import com.parkauto.client.entity.Client;
 import com.parkauto.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation;
 
 import java.util.List;
 
